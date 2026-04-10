a8aa139f653382e4be33b431946176067ecae75a
originEdu <originpark82@gmail.com>
2026-04-10 13:40:02 +0900

    Update
    1. C++ class 설정
    2. 빌드 기본 세팅

---
+++ Source/UnrealCpp0410/MyGameModeBase.cpp
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameModeBase.h"
+
+AMyGameModeBase::AMyGameModeBase()
+{
+	UE_LOG(LogTemp, Warning, TEXT("안녕하세요"));
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, TEXT("Hello World"));
+	}
+	
+}
---
+++ Source/UnrealCpp0410/UnrealCpp0410.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UnrealCpp0410.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealCpp0410, "UnrealCpp0410" );
